2d979a29ac2da36314f988de556e2611e4951996
David Urting <david.urting@live.com>
2020-05-20 10:02:32 +0200

    initial version

---
+++ application/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import "@company/some-package";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'application';
+}
